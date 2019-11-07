f6ccfe98b9eef8027be415783e35c92c9e47379b
daneklustig <danek.lustig@gmail.com>
2019-11-07 15:43:39 +0100

    done

--- starter_code/app.js
+++ starter_code/app.js
@@ -1,10 +1,12 @@
-
 const express = require('express');
-const hbs     = require('hbs');
-const app     = express();
-const path    = require('path');
+const hbs = require('hbs');
+const app = express();
+const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
 const punkAPI = new PunkAPIWrapper();
+
+// partials are templates within a template
+hbs.registerPartials(__dirname + "/views/partials");
 
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
@@ -14,6 +16,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.render('index');
+});
+
+
+app.get('/beers', (req, res, next) => {
+
+  punkAPI.getBeers()
+    .then(beersList => {
+      // console.log(beersList);
+      res.render('beers', {
+        beersList
+      });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+});
+
+app.get('/randombeer', (req, res, next) => {
+
+  // getRandom always give you an array with one object, every time we refresh the page
+  punkAPI.getRandom()
+    .then(randomBeer => {
+
+      console.log(randomBeer)
+      res.render('randombeer', {randomBeer});
+    })
+    .catch(error => {
+      console.log(error)
+    })
 });
 
 
